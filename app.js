e328ffd479ae17987f9ef2126b1d38182819333f
fuyz <953958160@qq.com>
2021-07-01 11:19:05 +0800

    云函数开发配置

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // app.js
 App({
-  onLaunch() {
+  onLaunch () {
+    wx.cloud.init({
+      traceUser: true,
+    })
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -10,8 +13,24 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res)
       }
     })
+
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'ff',
+      // 传给云函数的参数
+      data: {
+        a: 1,
+        b: 2,
+      },
+    })
+      .then(res => {
+        console.log(res.result)
+      })
+      .catch(console.error)
+
   },
   globalData: {
     userInfo: null
